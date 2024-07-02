9a6d6e61585f3b18f5b6894ecedae43754b741ab
Gustavo <gp67009@gmail.com>
2024-07-02 20:00:01 -0300

    Atualizando serviços mobile

--- src/pages/Servicos.jsx
+++ src/pages/Servicos.jsx
@@ -1,12 +1,20 @@
 import '../assets/css/Servicos.css'
 import cozinha from '../../public/img/cozinha.png'
+import cozinhaMobile from '../../public/img/cozinha-mobile.svg'
 import iluminacao from '../../public/img/iluminacao.png'
+import iluminacaoMobile from '../../public/img/iluminacao-mobile.svg'
 import ambientacao from '../../public/img/ambientacao.png'
+import ambientacaoMobile from '../../public/img/ambientacao-mobile.svg'
 import controle from '../../public/img/controle.png'
+import controleMobile from '../../public/img/controle-mobile.svg'
 import sala from '../../public/img/sala.png'
+import salaMobile from '../../public/img/sala-mobile.svg'
 import rede from '../../public/img/rede.png'
+import redeMobile from '../../public/img/rede-mobile.svg'
 import wifi from '../../public/img/wifi.png'
+import wifiMobile from '../../public/img/wifi-mobile.svg'
 import seguranca from '../../public/img/seguranca.png'
+import segurancaMobile from '../../public/img/seguranca-mobile.svg'
 import { Link } from 'react-router-dom'
 import whatsapp from "/img/whatsapp-icon.png";
 
@@ -23,6 +31,10 @@ export function Servico() {
                 <div className="nossoServicos">
                     <h1>NOSSOS SERVIÇOS</h1>
                     <h5>Saiba mais sobre o que você encontrará em nossa empresa</h5>
+                </div>
+
+                <div className="imgConzinha-mobile">
+                    <img src={cozinhaMobile} alt="" />
                 </div>
 
             </section>
@@ -51,12 +63,12 @@ export function Servico() {
 
                 <div className="grupo2">
                     <div className='cartaoQuatro'>
-                    <Link to="/sala" id='img1'><img src={sala} alt="" /></Link>
-                    <Link to="/sala"><h2>Sala de Cinema <br /> Personalizada</h2></Link>
+                        <Link to="/sala" id='img1'><img src={sala} alt="" /></Link>
+                        <Link to="/sala"><h2>Sala de Cinema <br /> Personalizada</h2></Link>
                     </div>
                     <div className='cartaoCinco'>
                         <Link to="/rede" id='img1'><img src={rede} alt="" /></Link>
-                        <Link to="/rede"><h2>Rede Cabeada</h2></Link>    
+                        <Link to="/rede"><h2>Rede Cabeada</h2></Link>
                     </div>
                     <div className='cartaoSeis'>
                         <Link to="/wifi" id='img1'><img src={wifi} alt="" /></Link>
@@ -72,11 +84,34 @@ export function Servico() {
 
             </section>
 
+            <section className="tres-mobile">
+
+                <div className='cartaoUm-mobile'>
+                    <Link to="/iluminacao"><img src={iluminacaoMobile} alt="" /></Link>
+                    <Link to="/ambientacao"><img src={ambientacaoMobile} alt="" /></Link>
+                </div>
+
+                <div className='cartaoUm-mobile'>
+                    <Link to="/controle"><img src={controleMobile} alt="" /></Link>
+                    <Link to="/sala"><img src={salaMobile} alt="" /></Link>
+                </div>
+
+                <div className='cartaoUm-mobile'>
+                    <Link to="/rede"><img src={redeMobile} alt="" /></Link>
+                    <Link to="/wifi"><img src={wifiMobile} alt="" /></Link>
+                </div>
+
+                <div className='cartaoUm-mobile'>
+                    <Link to="/seguranca"><img src={segurancaMobile} alt="" /></Link>
+                </div>
+
+            </section>
+
             <div className='fixed-bottom right-100 p-3' >
                 <a id='botaoFixo' href="https://web.whatsapp.com/send?phone=11966413001" target='_blank' rel='noopener noreferrer'>
                     <img src={whatsapp} className="custom-whatsapp-img" alt="WhatsApp" />
                 </a>
-            </div>       
+            </div>
 
         </>
     )
